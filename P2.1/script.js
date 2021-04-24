0612603ec66793e72bd52fe606819bbd3b186cd9
BenniP <benjamin.reinhold.pipus@hs-hurtwangen.de>
2021-04-24 19:10:55 +0200

    Aufgabe Final
    2.1 finish

--- P2.1/script.js
+++ P2.1/script.js
@@ -1,19 +1,31 @@
 "use strict";
-/*function a1(): void {
-    let x: string = "Alles";
+function a1() {
+    let x = "Alles";
     console.log(x);
-    func1();
+    func8();
     console.log("Logo!");
 }
-
-a1();
-
-function func1(): void {
+function func8() {
     console.log("Klar?");
 }
-
-
-1)
+//a1();
+let u = "Alles";
+function b1() {
+    b2();
+    b3();
+    b4();
+}
+function b2() {
+    console.log(u + " Gute!");
+}
+function b3() {
+    console.log(u + " Klar?");
+}
+function b4() {
+    console.log(u + " Logo!");
+}
+b1();
+/*1)
 
 a)
 Alles
@@ -27,50 +39,46 @@ Variablenname sind nur Strings zulässig, int geht nicht
 b)
 zuerst wird eine Funktion a1() erstellt, hier wird die Variable x mit dem String "Alles" belegt, dann wird dieser String auf der Console ausgegeben. Anschließend wird die funktion func1() ausgeführt, welche den String "Klar?" auf der
 Console ausgibt. Am Ende wird auf der Console "Logo!" ausgegeben. Das Alles ist noch teil der Funktion a1(), d.h. es wird erst alles ausgeführt, sobald die Funktion a1() auch aufgerufen wird,
-was in Zeile 8 geschieht. Bis dahin passier absolut nichts.*/
-//c)
-/*function a2(): void {
-    let i: number = 9;
+was in Zeile 8 geschieht. Bis dahin passier absolut nichts.
 
+//c)
+*/
+function a2() {
+    let i = 9;
     do {
         console.log(i);
         i = i - 1;
-    } while ( i > 0); Code hier in der Aufgabenstellung fehlerhaft (whitespace bei der While-Schleife fehlt)
+    } while (i > 0);
 }
-
 a2();
-
-
+/*
 2)
 es wird ein integer i = 9 definiert, hier wird eine do-while schleife ausgeführt, bei welcher der wert von i auf der console ausgegeben wird und anschließen und 1 verringert wird.
-Dieser loop läuft so lange bis i nicht mehr die Condition des While Statements erfüllt. Das while muss am Ende der Schleife stehen.*/
-/*
-3)
-a) Ja, was falsch ist, lässt sich aus den Fehlermeldungen schließen.*/
-/*
-let x: string = "Hallo";
-console.log(x);     //Hallo
-func1(x);           //Bla
-console.log(x);     //Hallo
-func2();            //Blubb
-func3();            //Hier wird x überschrieben
-console.log(x);     //Test
+Dieser loop läuft so lange bis i nicht mehr die Condition des While Statements erfüllt. Das while muss am Ende der Schleife stehen.
 
-function func1(y: string): void {
+
+3)
+a) Ja, was falsch ist, lässt sich aus den Fehlermeldungen schließen.
+*/
+let x = "Hallo";
+console.log(x); //Hallo
+func1(x); //Bla
+console.log(x); //Hallo
+func2(); //Blubb
+func3(); //Hier wird x überschrieben
+console.log(x); //Test 
+function func1(y) {
     y = "Bla"; //Lokale Variable
     console.log(y);
 }
-
-function func2(): void {
-    let x: string = "Blubb";
+function func2() {
+    let x = "Blubb";
     console.log(x);
 }
-
-function func3(): void {
+function func3() {
     x = "Test";
 }
-
-
+/*
 4)
 
 a) Vermutung:
@@ -87,21 +95,19 @@ als Übergabeparameter in sich selbst weiter zu verarbeiten,
 wobei die globale dann als lokale Variable innerhalb der Funktion gesehen wird und somit nach dem Ende dieser, "stirbt".
 Variablen dagegen sind Werte eines bestimmten Types (int/String/etc.), die von Funktionen
 verarbeitet werden können (Übergabeparameter).
-*/
+
+
+
 // 5)
 // a)
-/*
-function multiply(x: number, y: number): number {
+*/
+function multiply(x, y) {
     console.log(x * y);
     return x * y;
 }
-
-multiply(6 , 3);
-
-
+multiply(6, 3);
 //b)
-
-function max(x: number, y: number): number {
+function max(x, y) {
     if (x === y) {
         console.log("Die Zahlen sind gleich");
         return 0;
@@ -115,57 +121,39 @@ function max(x: number, y: number): number {
         return y;
     }
 }
-
-max(4 , 2);
-
-
+max(4, 2);
 //c)
-
-function count(): void {
-    let a: number = 0;
-    let i: number = 1;
-    
+function count() {
+    let a = 0;
+    let i = 1;
     do {
         a = a + i;
         console.log(a); // Gibt Zwischensumme aus
         i = i + 1;
     } while (i < 101);
 }
-
 count();
-
-
 //d)
-
-function random(min: number, max: number): void {
+function random(min, max) {
     for (let i = 0; i < 10; i++) {
-        console.log(Math.random() * (max - min) + min) ;
+        console.log(Math.random() * (max - min) + min);
     }
 }
-
 random(0, 100);
-
-
-
-function factorial(n: number): number {
+function factorial(n) {
     var result = n;
     if (n <= 1)
-      return 1;
+        return 1;
     while (n > 1) {
-      n--;
-      result = result * n;
-      console.log(result);
+        n--;
+        result = result * n;
+        console.log(result);
     }
     return result;
 }
-
 factorial(4);
-*/
 function leapyears() {
     for (let i = 1900; i < 2021; i++) {
-        /*if (((i / 4) % 0) && !((i / 100) % 0) ) {
-            console.log(i);
-        }*/
         if (i % 4 == 0) {
             if (!(i % 100 == 0)) {
                 console.log(i);
@@ -177,4 +165,71 @@ function leapyears() {
     }
 }
 leapyears();
+//6)
+//a)
+function hashtag() {
+    let hash = "#";
+    for (let i = 0; i < 7; i++) {
+        console.log(hash);
+        hash += "#";
+    }
+}
+hashtag();
+//b)
+function fizzbuzz() {
+    for (let i = 1; i < 101; i++) {
+        if (i % 3 == 0) {
+            let f = "Fizz";
+            console.log(f);
+        }
+        else if (!(i % 3 == 0) && i % 5 == 0) {
+            let b = "Buzz";
+            console.log(b);
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+fizzbuzz();
+//c)
+function fizzbuzz2() {
+    for (let i = 1; i < 101; i++) {
+        if (i % 15 == 0) {
+            let fb = "FizzBuzz";
+            console.log(fb);
+        }
+        else if (i % 3 == 0) {
+            let f = "Fizz";
+            console.log(f);
+        }
+        else if (!(i % 3 == 0) && i % 5 == 0) {
+            let b = "Buzz";
+            console.log(b);
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+fizzbuzz2();
+//d) e)
+function schach(b, h) {
+    let z = "";
+    for (let s = 0; s < h; s++) { // höhe
+        if (s % 2 == 0)
+            z += " ";
+        for (let i = 0; i < b; i++) { // breite
+            if (i % 2 == 0) {
+                z += "#";
+            }
+            else {
+                z += " ";
+            }
+        }
+        z += "\n"; // z = z + "\n";
+    }
+    console.log(z);
+}
+schach(8, 8);
 //# sourceMappingURL=script.js.map
--- P2.1/script.ts
+++ P2.1/script.ts
@@ -1,18 +1,35 @@
-/*function a1(): void {
+function a1(): void {
     let x: string = "Alles";
     console.log(x);
-    func1();
+    func8();
     console.log("Logo!");
 }
-
-a1();
-
-function func1(): void {
+function func8(): void {
     console.log("Klar?");
 }
+//a1();
 
 
-1)
+let u: string = "Alles";
+
+function b1(): void {
+    b2();
+    b3();
+    b4();
+}
+function b2(): void {
+    console.log(u + " Gute!");
+}
+function b3(): void {
+    console.log(u + " Klar?");
+}
+function b4(): void {
+    console.log(u + " Logo!");
+}
+
+b1();
+
+/*1)
 
 a)
 Alles
@@ -26,36 +43,33 @@ Variablenname sind nur Strings zulässig, int geht nicht
 b)
 zuerst wird eine Funktion a1() erstellt, hier wird die Variable x mit dem String "Alles" belegt, dann wird dieser String auf der Console ausgegeben. Anschließend wird die funktion func1() ausgeführt, welche den String "Klar?" auf der
 Console ausgibt. Am Ende wird auf der Console "Logo!" ausgegeben. Das Alles ist noch teil der Funktion a1(), d.h. es wird erst alles ausgeführt, sobald die Funktion a1() auch aufgerufen wird,
-was in Zeile 8 geschieht. Bis dahin passier absolut nichts.*/
+was in Zeile 8 geschieht. Bis dahin passier absolut nichts.
 
 //c)
+*/
 
 
-
-
-/*function a2(): void {
+function a2(): void {
     let i: number = 9;
 
     do {
         console.log(i);
         i = i - 1;
-    } while ( i > 0); Code hier in der Aufgabenstellung fehlerhaft (whitespace bei der While-Schleife fehlt)
+    } while ( i > 0);
 }
 
 a2();
 
 
+/*
 2)
 es wird ein integer i = 9 definiert, hier wird eine do-while schleife ausgeführt, bei welcher der wert von i auf der console ausgegeben wird und anschließen und 1 verringert wird.
-Dieser loop läuft so lange bis i nicht mehr die Condition des While Statements erfüllt. Das while muss am Ende der Schleife stehen.*/
+Dieser loop läuft so lange bis i nicht mehr die Condition des While Statements erfüllt. Das while muss am Ende der Schleife stehen.
 
 
-
-
-/*
 3)
-a) Ja, was falsch ist, lässt sich aus den Fehlermeldungen schließen.*/
-/*
+a) Ja, was falsch ist, lässt sich aus den Fehlermeldungen schließen.
+*/
 let x: string = "Hallo";
 console.log(x);     //Hallo
 func1(x);           //Bla
@@ -78,7 +92,7 @@ function func3(): void {
     x = "Test";
 }
 
-
+/*
 4)
 
 a) Vermutung:
@@ -95,12 +109,12 @@ als Übergabeparameter in sich selbst weiter zu verarbeiten,
 wobei die globale dann als lokale Variable innerhalb der Funktion gesehen wird und somit nach dem Ende dieser, "stirbt". 
 Variablen dagegen sind Werte eines bestimmten Types (int/String/etc.), die von Funktionen
 verarbeitet werden können (Übergabeparameter).
-*/
+
 
 
 // 5)
 // a)
-/*
+*/
 function multiply(x: number, y: number): number {
     console.log(x * y);
     return x * y;
@@ -170,13 +184,10 @@ function factorial(n: number): number {
 }
 
 factorial(4);
-*/
+
 
 function leapyears(): void {
     for (let i = 1900; i < 2021; i++) {
-        /*if (((i / 4) % 0) && !((i / 100) % 0) ) {
-            console.log(i);
-        }*/
     if (i % 4 == 0) {
         if (!(i % 100 == 0)) {
             console.log(i);
@@ -187,3 +198,76 @@ function leapyears(): void {
  }
 }
 leapyears();
+
+
+//6)
+//a)
+function hashtag(): void {
+   let hash: String = "#";
+   for (let i = 0; i < 7; i++) {
+       console.log(hash);
+       hash += "#";
+   }
+}
+
+hashtag();
+
+//b)
+
+function fizzbuzz(): void {
+    for (let i = 1; i < 101; i++) {
+        if (i % 3 == 0) {
+            let f: String = "Fizz";
+            console.log(f);
+        } else if (!(i % 3 == 0) && i % 5 == 0) {
+            let b: String = "Buzz";
+            console.log(b);
+        } else {
+            console.log(i);
+        }
+    }
+}
+fizzbuzz();
+
+//c)
+
+function fizzbuzz2(): void {                      // Lösbar mit gemeinsamen Nenner 15 ODER mit verschachtelten If-Statements, wobei der gewählte Weg einfacher ist.
+    for (let i = 1; i < 101; i++) {
+        if (i % 15 == 0) {
+            let fb: String = "FizzBuzz";
+            console.log(fb);
+        }
+        else if (i % 3 == 0) {
+            let f: String = "Fizz";
+            console.log(f);
+        } else if (!(i % 3 == 0) && i % 5 == 0) {
+            let b: String = "Buzz";
+            console.log(b);
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzbuzz2();
+
+//d) e)
+
+function schach(b: number, h: number): void {
+    let z: String = "";
+    for (let s = 0; s < h; s++) { // höhe
+        if (s % 2 == 0)
+            z += " ";
+        for (let i = 0; i < b; i++) { // breite
+            if (i % 2 == 0) {
+                z += "#";
+            } else {
+                z += " ";
+            }
+        }
+        z += "\n"; // z = z + "\n";
+    }
+    console.log(z);
+}
+
+schach(8, 8);
