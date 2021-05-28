f7a0344f9a181a8e8a8f7e6e5659e6dff74dd5be
BenniP <benjamin.reinhold.pipus@hs-hurtwangen.de>
2021-05-28 17:07:21 +0200

    P3.1

---
+++ P3.1/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var Aufgabe3_1;
+(function (Aufgabe3_1) {
+    let button = document.getElementById("button");
+    button.addEventListener("click", handle);
+    async function handle() {
+        let formData = new FormData(document.forms[0]);
+        let _url = "https://bennihirokugis.herokuapp.com/";
+        let query = new URLSearchParams(formData);
+        _url = _url + "?" + query.toString();
+        await fetch(_url);
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+        let response = await fetch(_url);
+        let answer = await response.text();
+        console.log(answer);
+        let paragraph = document.createElement("p");
+        paragraph.innerText = answer;
+        document.body.appendChild(paragraph);
+    }
+})(Aufgabe3_1 || (Aufgabe3_1 = {}));
+//# sourceMappingURL=index.js.map
---
+++ P3.1/index.ts
@@ -0,0 +1,25 @@
+namespace Aufgabe3_1 {
+    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    button.addEventListener("click", handle);
+
+    async function handle(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let _url: RequestInfo = "https://bennihirokugis.herokuapp.com/"; 
+        let query: URLSearchParams = new URLSearchParams (<any>formData);
+        _url = _url + "?" + query.toString();
+        await fetch(_url);
+
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+            
+        }
+        let response: Response = await fetch(_url);
+        let answer: string = await response.text();
+        console.log(answer);
+        let paragraph: HTMLParagraphElement = document.createElement("p");
+        paragraph.innerText = answer;
+        document.body.appendChild(paragraph);
+    }
+}
