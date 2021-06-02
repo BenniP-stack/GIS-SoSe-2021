f3ae0b05d8fa918c0f52b8ffaaca205a024bafdd
BenniP <benjamin.reinhold.pipus@hs-hurtwangen.de>
2021-06-02 14:45:13 +0200

    P3.2
    First try

---
+++ P3.2/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var Aufgabe3_2;
+(function (Aufgabe3_2) {
+    let buttonJSON = document.getElementById("buttonJSON");
+    buttonJSON.addEventListener("click", handlejson);
+    let buttonHTML = document.getElementById("buttonHTML");
+    buttonHTML.addEventListener("click", handlehtml);
+    let server = document.getElementById("server");
+    async function handlehtml() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://bennihirokugis.herokuapp.com/";
+        url += "/html";
+        let query = new URLSearchParams(formData);
+        url = url += "?" + query.toString();
+        let response = await fetch(url);
+        let responseText = await response.text();
+        server.innerHTML = responseText;
+    }
+    async function handlejson() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://bennihirokugis.herokuapp.com/";
+        url += "/json";
+        let query = new URLSearchParams(formData);
+        url = url += "?" + query.toString();
+        let response = await fetch(url);
+        let responseText = await response.text();
+        console.log(responseText);
+        let responseJSON = JSON.parse(responseText);
+        console.log(responseJSON);
+        server.innerHTML = responseText;
+        console.log(server);
+    }
+})(Aufgabe3_2 || (Aufgabe3_2 = {}));
+//# sourceMappingURL=index.js.map
---
+++ P3.2/index.ts
@@ -0,0 +1,35 @@
+namespace Aufgabe3_2 {
+    let buttonJSON: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonJSON");
+    buttonJSON.addEventListener("click", handlejson);
+    let buttonHTML: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonHTML");
+    buttonHTML.addEventListener("click", handlehtml);
+
+    let server: HTMLElement = <HTMLElement>document.getElementById("server");
+
+    async function handlehtml(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://bennihirokugis.herokuapp.com/";
+        url += "/html";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url += "?" + query.toString();
+        let response: Response = await fetch(url);
+        let responseText: string = await response.text();
+        server.innerHTML = responseText;
+    }
+
+    async function handlejson(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://bennihirokugis.herokuapp.com/";
+        url += "/json";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url += "?" + query.toString();
+        let response: Response = await fetch(url);
+        let responseText: string = await response.text();
+        console.log(responseText);
+        let responseJSON: JSON = JSON.parse(responseText);
+        console.log(responseJSON);
+
+        server.innerHTML = responseText;
+        console.log(server);
+    }
+}
